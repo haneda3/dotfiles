78e5ab729dfa413208d26e1917feb7a14e9fdf14
masahito.mizoguchi <masahito.mizoguchi@mjwdev0001.(none)>
2013-05-13 18:42:45 +0900

    add neobundle install process

--- setup.sh
+++ setup.sh
@@ -1,3 +1,12 @@
+if [ ! -d ~/.vim/bundle ]; then
+  mkdir -p ~/.vim/bundle
+  git clone git@github.com:Shougo/neobundle.vim.git ~/.vim/bundle/neobundle.vim
+fi
+
+echo ""
+echo "please execute :NeoBundleInstall on vim"
+echo ""
+
 ln -s ~/dotfiles/.vimrc ~/.vimrc
 ln -s ~/dotfiles/.inputrc ~/.inputrc
 
